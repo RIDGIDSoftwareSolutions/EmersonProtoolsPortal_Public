485c4fa6d6196e61048eb81f2eac6b3b4c21e14e
Gerry Butler <Gerry.Butler@Emerson.com>
2019-05-10 18:35:09 -0400

    1.5-SNAPSHOT Added a new Exception Type for Primary Key Incompatibility

---
+++ com.ridgid.oss.orm/src/main/java/com/ridgid/oss/orm/SuffixedPrimaryKeyIncompatibleException.java
@@ -0,0 +1,23 @@
+package com.ridgid.oss.orm;
+
+/**
+ * Thrown when an attempt is made to add an entity to the child collection of a given entity
+ * that has a primary key that is supposed to be prefixed by the parent key and is not
+ *
+ * For example, if Order has an Customer Number and Order number as its Primary Key and
+ * it has a collection of Order Lines that is supposed to have Customer Number, Order Number, and Line Number
+ * as its Primary Key and the Order Line that is being attempted to add to the collection has a different
+ * Customer Number and/or Order Number from the Parent, this exception could/should be thrown.
+ */
+public class SuffixedPrimaryKeyIncompatibleException extends RuntimeException {
+    public SuffixedPrimaryKeyIncompatibleException() {
+    }
+
+    public SuffixedPrimaryKeyIncompatibleException(String message) {
+        super(message);
+    }
+
+    public SuffixedPrimaryKeyIncompatibleException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
