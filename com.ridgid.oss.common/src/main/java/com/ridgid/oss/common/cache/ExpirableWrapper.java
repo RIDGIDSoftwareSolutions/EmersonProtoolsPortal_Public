5506965a4fda973191e2d3fb5b316d2f00b03f34
Gerry Butler <Gerry.Butler@Emerson.com>
2019-09-27 10:28:46 -0400

    - Added JavaDoc comments

--- com.ridgid.oss.common/src/main/java/com/ridgid/oss/common/cache/ExpirableWrapper.java
+++ com.ridgid.oss.common/src/main/java/com/ridgid/oss/common/cache/ExpirableWrapper.java
@@ -1,11 +1,28 @@
 package com.ridgid.oss.common.cache;
 
+/**
+ * Wrapper for any object that will add the expirable interface needed by the ExpirableCache interface
+ *
+ * @param <T> object type to wrap
+ * @code {
+ * ExpirableCache<Integer,ExpirableWrapper<String>> cache = new InMemoryExpirableCache<>();
+ * ...
+ * cache.add( 3, new ExpirableWrapper( "Hello" ) );
+ * }
+ */
 @SuppressWarnings({"unused", "SpellCheckingInspection"})
 public class ExpirableWrapper<T> implements Expirable
 {
     private final T    wrapped;
     private final long expirationTimeMillis;
 
+    /**
+     * Create an ExpirableWrapper for T object that will expire at the given expirationTimeMillis with respect to the
+     * System time (System.currentTimeMillis())
+     *
+     * @param wrapped the object to wrap in the ExpirableWrapper
+     * @param expirationTimeMillis the system time in milliseconds to expire
+     */
     public ExpirableWrapper(T wrapped,
                             long expirationTimeMillis)
     {
@@ -19,6 +36,11 @@ public class ExpirableWrapper<T> implements Expirable
         return expirationTimeMillis < System.currentTimeMillis();
     }
 
+    /**
+     * Unwraps and returns the wrapped object
+     *
+     * @return the object of type T that was wrapped
+     */
     public T unwrap()
     {
         return wrapped;
---
+++ com.ridgid.oss.common/src/main/java/com/ridgid/oss/common/cache/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides classes and interfaces for caching of various object types under various keys.
+ * <p>
+ * The classes and interfaces in this package are designed generically to work with any key and value type
+ * that is appropriate to the type of cache.
+ */
+package com.ridgid.oss.common.cache;
