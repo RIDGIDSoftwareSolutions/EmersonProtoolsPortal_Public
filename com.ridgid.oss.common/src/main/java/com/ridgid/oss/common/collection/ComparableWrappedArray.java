1b05f1fb6c0f119364ee1d77d76da175be6bc245
Gerry Butler <Gerry.Butler@Emerson.com>
2019-09-27 15:15:28 -0400

    - Fixed JavaDoc comment - Fixed compilation error - Added equals/hascode to ComparabledWrappedArray

--- com.ridgid.oss.common/src/main/java/com/ridgid/oss/common/collection/ComparableWrappedArray.java
+++ com.ridgid.oss.common/src/main/java/com/ridgid/oss/common/collection/ComparableWrappedArray.java
@@ -5,7 +5,7 @@ import com.ridgid.oss.common.helper.ComparisonHelpers;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
-@SuppressWarnings("WeakerAccess")
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class ComparableWrappedArray<T extends Comparable<T>>
     implements Comparable<ComparableWrappedArray<T>>
 {
@@ -22,5 +22,18 @@ public class ComparableWrappedArray<T extends Comparable<T>>
     @Override
     public int compareTo(ComparableWrappedArray<T> o) {
         return ComparisonHelpers.comparingNullsLast(unwrap(), o.unwrap());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        ComparableWrappedArray<?> that = (ComparableWrappedArray<?>) o;
+        return Arrays.equals(array, that.array);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(array);
     }
 }
